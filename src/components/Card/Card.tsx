046ba57c08267f9bfb0cb40d77a83208ae5b9c01
Daniil Surin <wkingsurin@gmail.com>
2025-05-26 19:25:47 +0500

    feat, style: Add Card state, onClick handler and update styles

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,15 +1,26 @@
 import "./Card.scss";
+import { useState, type MouseEventHandler } from "react";
 
 type Props = {
 	children: string;
 	animate?: boolean;
 };
 
+const CARD = {
+	status: "closed",
+};
+
 export default function Card({ animate, children }: Props) {
+	const [card, setCard] = useState({ ...CARD, image: children });
+
+	const onClick: MouseEventHandler = () => {
+		setCard(() => ({ ...card, status: "waiting" }));
+	};
+
 	return (
-		<div className="card">
-			{animate && <div className="border">{children}</div>}
-			<div className="content"></div>
+		<div className="card" onClick={onClick}>
+			{animate && <div className="border"></div>}
+			<div className="content">{card.status !== "closed" && card.image}</div>
 		</div>
 	);
 }
