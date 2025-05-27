9e679afabdada1dc00197cf88572b7ac1bda9a14
Daniil Surin <wkingsurin@gmail.com>
2025-05-27 20:45:37 +0500

    fix: Update SMILES from array to object with LVL fields

--- src/constants/smiles.ts
+++ src/constants/smiles.ts
@@ -1,4 +1,6 @@
-export const SMILES: string[] = [
+import { shuffle, doubleArray } from "../utils/utils";
+
+export const emoji: string[] = [
 	"😁",
 	"😂",
 	"😃",
@@ -17,8 +19,12 @@ export const SMILES: string[] = [
 	"😐",
 	"😒",
 	"😓",
-	"😔",
-	"😖",
 	"😘",
 	"😚",
 ];
+
+export const SMILES: Record<string, string[]> = {
+	LVL1: shuffle(doubleArray(emoji.slice(0, 9))),
+	LVL2: shuffle(doubleArray(emoji.slice(0, 14))),
+	LVL3: shuffle(doubleArray(emoji)),
+};
