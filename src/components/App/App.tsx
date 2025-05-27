525caf2f335bedafe3203a3baf6f0eb428a892dd
Daniil Surin <wkingsurin@gmail.com>
2025-05-27 21:38:50 +0500

    feat: Add level state and handler handleChangeLevel, pass state and handler to StateMenu and GameGrid component props
    - Added level state
    - Added handler handleChangeLevel to change level state
    - Passed state and handler to StateMenu and GameGrid components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,19 +4,30 @@ import GameGrid from "../GameGrid";
 import Modal from "../Modal";
 import Overlay from "../Overlay/Overlay";
 
-import { useState } from "react";
+import { useState, type MouseEventHandler } from "react";
 
 export default function App() {
+	const [level, setLevel] = useState(1);
 	const [open, setOpen] = useState(false);
 
 	const handleClose = (): void => {
 		setOpen(() => false);
 	};
 
+	const handleChangeLevel: MouseEventHandler = (e) => {
+		const target = e.target as HTMLButtonElement;
+		if (!target.name) return;
+
+		setLevel(() => Number(target.name));
+	};
+
 	return (
 		<div className="app">
-			<StartMenu></StartMenu>
-			<GameGrid></GameGrid>
+			<StartMenu
+				level={level}
+				handleChangeLevel={handleChangeLevel}
+			></StartMenu>
+			<GameGrid level={level}></GameGrid>
 			<Overlay open={open}>
 				<Modal value={23} onClick={handleClose}></Modal>
 			</Overlay>
