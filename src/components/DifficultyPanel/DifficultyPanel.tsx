aa83f2141ee6d37478f9d01d2de390deda8554c6
Daniil Surin <wkingsurin@gmail.com>
2025-05-27 21:23:13 +0500

    feat: Update DifficultyPanel component type, add callback handleChangeLevel to props and update name value
    - Updated DifficultyPanel component type
    - Updated name value
    - Added callback handleChangeLevel to switch level

--- src/components/DifficultyPanel/DifficultyPanel.tsx
+++ src/components/DifficultyPanel/DifficultyPanel.tsx
@@ -1,20 +1,25 @@
 import "./DifficultyPanel.scss";
 import Button from "../Button";
+import type { MouseEventHandler } from "react";
 
-type Props = {
+type DifficultyProps = {
 	difficulty?: string;
+	handleChangeLevel: MouseEventHandler;
 };
 
-export default function DifficultyPanel({ difficulty }: Props) {
+export default function DifficultyPanel({
+	difficulty,
+	handleChangeLevel,
+}: DifficultyProps) {
 	return (
-		<div className="difficulty-panel">
-			<Button difficulty={difficulty} animate name="3x6">
+		<div className="difficulty-panel" onClick={handleChangeLevel}>
+			<Button difficulty={difficulty} animate name="1">
 				3x6
 			</Button>
-			<Button difficulty={difficulty} animate name="4x7">
+			<Button difficulty={difficulty} animate name="2">
 				4x7
 			</Button>
-			<Button difficulty={difficulty} animate name="5x8">
+			<Button difficulty={difficulty} animate name="3">
 				5x8
 			</Button>
 		</div>
