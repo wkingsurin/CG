9a88d0fd036fe3641671679f1acca882b79603ec
Daniil Surin <wkingsurin@gmail.com>
2025-06-01 18:35:03 +0500

    feat, fix: Updated and extended GameGrid typing and pass prop to Card
    - Updated and extended GameGrid component typing
    - Passed props to Card: grid and setGrid

--- src/components/GameGrid/GameGrid.tsx
+++ src/components/GameGrid/GameGrid.tsx
@@ -1,18 +1,27 @@
 import "./GameGrid.scss";
 import Card from "../Card";
+import type React from "react";
 
 type GridProps = {
-	grid: {emoji: string, id: number}[];
+	grid: { emoji: string; status: string; id: number }[];
+	setGrid: React.Dispatch<
+		React.SetStateAction<{ emoji: string; status: string; id: number }[]>
+	>;
 };
 
-export default function GameGrid({ grid }: GridProps) {
-	const gridColumns: number = grid.length === 18 ? 6 : grid.length === 28 ? 7 : 8;
+export default function GameGrid({ grid, setGrid }: GridProps) {
+	const gridColumns: number =
+		grid.length === 18 ? 6 : grid.length === 28 ? 7 : 8;
 
 	const cards = grid.map((smile) => {
 		return (
-			<Card key={smile.id} animate>
-				{smile.emoji}
-			</Card>
+			<Card
+				key={smile.id}
+				grid={grid}
+				setGrid={setGrid}
+				card={smile}
+				animate
+			></Card>
 		);
 	});
 
